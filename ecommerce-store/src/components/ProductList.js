b13325150bd3c1540edc4cdd9f61781b7da6a89f
JDM0126 <joseph.maninang@precisiontechpartners.com>
2019-08-14 10:32:28 +0800

    initial files

---
+++ ecommerce-store/src/components/ProductList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
+
+export default class ProductList extends Component {
+    // static propTypes = {
+    //     prop: PropTypes
+    // }
+
+    render() {
+        return (
+            <div>
+                <h3>Hello from Product</h3>
+            </div>
+        )
+    }
+}
